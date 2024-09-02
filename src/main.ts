276ddb112a0ddfa98ab7f79b20b86236c583e3cf
Anthony Ge <geant@seas.upenn.edu>
2024-09-02 19:18:14 -0400

    test

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import Camera from './Camera';
 import {setGL} from './globals';
 import ShaderProgram, {Shader} from './rendering/gl/ShaderProgram';
 
+
 // Define an object with application parameters and button callbacks
 // This will be referred to by dat.GUI's functions that add GUI elements.
 const controls = {
